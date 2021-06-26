353e247c82d810d30ed27da270af5eef34b366ff
r-tamura <rtamura30@gmail.com>
2021-06-26 22:04:33 +0900

    chakuraui 素振り

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,20 @@
 <template>
-  <CList spacing="3" style-type="disc">
-    <CListItem v-for="content in contents" :key="content.id">
-      <nuxt-link :to="`/${content.id}`">
-        {{ content.title }}
-      </nuxt-link>
-    </CListItem>
-  </CList>
+  <CBox w="100%" p="4">
+    <CList spacing="3" style-type="disc">
+      <CListItem v-for="content in contents" :key="content.id">
+        <nuxt-link :to="`/${content.id}`">
+          {{ content.title }}
+        </nuxt-link>
+      </CListItem>
+    </CList>
+    <c-button>ボタン</c-button>
+  </CBox>
 </template>
 
 <script>
-import { CList, CListItem } from '@chakra-ui/vue'
+import { CBox, CButton, CList, CListItem } from '@chakra-ui/vue'
 export default {
-  components: { CList, CListItem },
+  components: { CBox, CButton, CList, CListItem },
   async asyncData({ $microcms }) {
     const data = await $microcms.get({
       endpoint: 'blog',
